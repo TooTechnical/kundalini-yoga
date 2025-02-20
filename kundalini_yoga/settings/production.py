05ac8a0583f1e99f3c12e51e60497fe35dccaffc
Dillon Malone <60452379+TooTechnical@users.noreply.github.com>
2025-02-20 23:29:15 +0000

    Switch to JawsDB MySQL

--- kundalini_yoga/settings/production.py
+++ kundalini_yoga/settings/production.py
@@ -3,12 +3,21 @@ import dj_database_url
 
 # Security settings
 DEBUG = False
-ALLOWED_HOSTS = ['kundalini-yoga-app-29d88017307f.herokuapp.com']
 
-# Database configuration
+ALLOWED_HOSTS = [
+    'kundalini-yoga-app-29d88017307f.herokuapp.com',
+    '.herokuapp.com'  # Wildcard for all Heroku subdomains
+]
+# MySQL Configuration
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        env='JAWSDB_URL',
+        conn_max_age=600,
+        engine='django.db.backends.mysql'
+    )
 }
+
+DATABASES['default']['OPTIONS'] = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
 
 # WhiteNoise configuration for static files
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
